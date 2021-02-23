bf1f8610c60060f0fc33d4b16cba86b4daf9459c
PrytMaks <prytkovmaks@gmail.com>
2021-02-23 15:01:31 +0500

    init: start task reverseNumber; add: final solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let strFromN = Math.abs(n)
+  strFromN = strFromN.toString();
+  let revStr = strFromN.split('').reverse().join('');
+  return +revStr;
 }
